83d9287329996c072d7d3115913a11647ba209b3
Salman Saputra <salmansaputra76@gmail.com>
2025-11-22 00:35:54 +0700

    Add logout functionality to welcome page for non-admin
    Introduced a logout form and button for authenticated users on the welcome page, allowing users to securely log out via POST request.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,6 +31,15 @@
                             >
                                 Dashboard
                             </a>
+                        @else
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+
+                                <a href="#" class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
+                                   onclick="event.preventDefault(); this.closest('form').submit();">
+                                    {{ __('Log Out') }}
+                                </a>
+                            </form>
                         @endif
                     @else
                         <a
