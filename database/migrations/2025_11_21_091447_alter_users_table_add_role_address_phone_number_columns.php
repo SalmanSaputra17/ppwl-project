bb0196c44623d1d4e3c9630b711bc3d7b2e838c4
Salman Saputra <salmansaputra76@gmail.com>
2025-11-21 16:39:32 +0700

    Add migrations for users, categories, products, and orders
    Introduces new migration files to extend the users table with role, address, and phone number columns, and to create categories, products, orders, and order_products tables. These migrations establish the foundational schema for an e-commerce system, including soft deletes and necessary foreign key relationships.

---
+++ database/migrations/2025_11_21_091447_alter_users_table_add_role_address_phone_number_columns.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->default('user');
+            $table->string('address')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['role', 'address', 'phone_number', 'deleted_at']);
+        });
+    }
+};
