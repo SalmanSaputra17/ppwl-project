63b0d2995e6bcb736dd94f5c2fc97f2c2c30ca3a
Salman Saputra <salmansaputra76@gmail.com>
2025-11-22 15:37:07 +0700

    Refactor dashboard and home views layout
    Updated dashboard.blade.php to use @extends and @section for layout consistency and improved markup. Commented out profit and sales cards in home.blade.php for simplification and maintainability.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,22 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+@extends('layouts.app')
+@section('title', 'Dashboard')
+@section('content')
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <div class="row">
+            <div class="col-lg-12 mb-4 order-0">
+                <div class="card">
+                    <div class="d-flex align-items-end row">
+                        <div class="col-sm-7">
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Selamat datang di halaman Dashboard UTS PPWL!</h5>
+                                <p class="mb-4">
+                                    Selamat bekerja <span class="fw-bold"></span> nikmati harimu dengan lebih baik
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
